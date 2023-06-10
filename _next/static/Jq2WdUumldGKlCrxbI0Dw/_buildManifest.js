3146c155eb7e8c7b8f329c2c7e624bd27e6b442e
sigridjineth <sigrid.jinhyung@gmail.com>
2023-06-10 16:38:45 +0900

    deploy to gh-pages

---
+++ _next/static/Jq2WdUumldGKlCrxbI0Dw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ff9bc380677ebcea.css","static/chunks/pages/index-bccf66b5f0c196d9.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
